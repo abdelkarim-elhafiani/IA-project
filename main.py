9d0fbb6b1e26169c6defe0a2830dee5c2df2607d
abdelali4 <abdelali.elhammadi@gmail.com>
2022-06-09 16:06:18 +0200

    adding read from youtube functionaliy

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from predictions.camera import initialize_caffe_models
 from predictions.camera import read_from_camera
 from predictions.image import read_from_image
+from predictions.youtube import read_from_youtube
 
 
 def print_hi(name):
@@ -13,6 +14,7 @@ def print_hi(name):
 if __name__ == '__main__':
 
     age_net,gender_net=initialize_caffe_models()
-    read_from_image(age_net,gender_net)
+    read_from_youtube(age_net,gender_net,
+    "https://www.youtube.com/watch?v=2ylVw5xBNEk&list=RD2ylVw5xBNEk&start_radio=1")
 
 
