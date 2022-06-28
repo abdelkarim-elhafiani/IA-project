1c9b4b8bbb9518007f1e91047cae4eff51ebbd17
abdelali4 <abdelali.elhammadi@gmail.com>
2022-06-29 00:09:29 +0200

    resolving some issues in the detection

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ if __name__ == '__main__':
     detection_robot.start()
 
     #detection_robot.read_from_camera()
-    #detection_robot.read_from_youtube("https://www.youtube.com/watch?v=C6ohRM48oAc&list=RDC6ohRM48oAc&start_radio=1")
+    #detection_robot.read_from_youtube(
+    # "https://www.youtube.com/watch?v=C6ohRM48oAc&list=RDC6ohRM48oAc&start_radio=1"
+    # )
     detection_robot.read_from_image("images/sample1.jpg")
     
