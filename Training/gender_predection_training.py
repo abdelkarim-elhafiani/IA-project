15681ce3d0facac23235814d8ef8660dd1c31f20
abdelkarim elhafiani <87866971+abdelkarim-elhafiani@users.noreply.github.com>
2022-06-28 21:09:30 +0100

    add a good training  gender model

---
+++ Training/gender_predection_training.py
@@ -0,0 +1,48 @@
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+import cv2
+from keras.models import Sequential,load_model,Model
+from keras.layers import Conv2D,MaxPool2D,Dense,Dropout,BatchNormalization,Flatten,Input
+from sklearn.model_selection import train_test_split
+
+from training.prepare_data_for_training import prepare_data_for_training
+
+gender,images = prepare_data_for_training("gender_data")
+
+gender = np.array(gender,dtype=np.int64)
+images = np.array(images)
+
+x_train_gender, x_test_gender, y_train_gender, y_test_gender = train_test_split(images, gender, random_state=42)
+
+
+gender_model = Sequential()
+
+gender_model.add(Conv2D(36, kernel_size=3, activation='relu', input_shape=(200,200,3)))
+
+gender_model.add(MaxPool2D(pool_size=3, strides=2))
+gender_model.add(Conv2D(64, kernel_size=3, activation='relu'))
+gender_model.add(MaxPool2D(pool_size=3, strides=2))
+
+gender_model.add(Conv2D(128, kernel_size=3, activation='relu'))
+gender_model.add(MaxPool2D(pool_size=3, strides=2))
+
+gender_model.add(Conv2D(256, kernel_size=3, activation='relu'))
+gender_model.add(MaxPool2D(pool_size=3, strides=2))
+
+gender_model.add(Conv2D(512, kernel_size=3, activation='relu'))
+gender_model.add(MaxPool2D(pool_size=3, strides=2))
+
+gender_model.add(Flatten())
+gender_model.add(Dropout(0.2))
+gender_model.add(Dense(512, activation='relu'))
+gender_model.add(Dense(1, activation='sigmoid', name='gender'))
+
+gender_model.compile(optimizer='adam',
+                        loss='binary_crossentropy',
+                        metrics=['accuracy'])
+
+history_gender = gender_model.fit(x_train_gender, y_train_gender,
+                        validation_data=(x_test_gender, y_test_gender), epochs=10)
+
+gender_model.save('models/gender_model.h5')
